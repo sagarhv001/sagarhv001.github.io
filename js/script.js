06a92d4c69e3f0cf3220780d47aef95ac5ba0e76
sagarhv001 <119564762+sagarhv001@users.noreply.github.com>
2024-02-12 22:18:56 +0530

    email  done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 
+}
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -31,7 +35,8 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 0);
 
     // remove toggle icon and navbar when click navbar links (scroll)
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
     // animation footer on scroll
 
